b3fef746c35c06c6b2dd9281f1cc7a0566b593f8
benglish00 <benglish00@users.noreply.github.com>
2017-11-02 15:01:15 -0600

    added challenges

--- STP_CH4.py
+++ STP_CH4.py
@@ -76,3 +76,75 @@ def add(x,y):
     :return: int sum of x andy
     """
     return(x+y)
+
+#Challenge 1. Define a function that squares a number
+def square_it(x=44):
+    """
+    Rertuns x ** 2
+    :param x: int
+    :return: x squared
+    """
+    return x**2
+
+print(square_it())
+
+#Challenge 2. Define a function that accepts a string as a parameter and prints it
+def write(x):
+    """
+    Returns none. Output string
+    :param x: any
+    :return: none
+    """
+    print(x)
+
+write("Colorado")
+
+#Challenge 3. Write a function with 1 required parapmeter and 2 optional
+def product(x,y=2,z=4):
+    """
+    Returns x * y * z
+    :param x: int
+    :param y: int
+    :param z: int
+    :return: product of x, y, and z
+    """
+    return(x*y*z)
+
+print(product(5,6,7))
+
+# Challenge 4. Write 2 functions are nd pass the 1st into the 2nd
+def half(x):
+    """
+    return x/2
+    :param x: int
+    :return: x / 2
+    """
+    return x/2
+
+def quad(x):
+    """
+    return x * 4
+    :param x: int
+    :return: x * 4
+    """
+    return(x*4)
+
+y = half(10)
+z = quad(y)
+print(y,z)
+
+#Error handle a string to float conversion
+def checknum(userString):
+    """
+    return userString as a float
+    :param userString: string
+    :return: float
+    """
+    try:
+        userString = float(userString)
+        return userString
+    except ValueError:
+        print("Function requires a number as a string")
+
+z = "50.2"
+print(checknum(z))
